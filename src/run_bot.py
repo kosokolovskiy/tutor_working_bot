b5dec3db56302d38a115197e81ede95935089f28
kosokolovsky <kosokolovsky@icloud.com>
2025-04-06 22:20:52 +0200

    logging

--- src/run_bot.py
+++ src/run_bot.py
@@ -4,13 +4,7 @@ from db_info.difference import DBAnalyzer
 from connectors.mysql import SQLConnector
 from main import doneOrNot
 import asyncio
-import threading
 import configparser
-
-
-# import nest_asyncio
-# nest_asyncio.apply()
-
 import logging
 import os
 
@@ -26,19 +20,17 @@ logging.basicConfig(
     ]
 )
 
-
 USERS, TOKEN, API_URL = get_creds()
 
 config = configparser.ConfigParser()
 config.read("creds.ini")
-
 
 dbname = config["MAIN"]["dbname"]
 username = config["MAIN"]["username"]
 password = config["MAIN"]["password"]
 rds_endpoint = config["MAIN"]["rds_endpoint"]
 
-app = MyBot.run_bot(TOKEN)
+obj = DBAnalyzer()
 
 
 def format_missing_tasks_markdown(missing_tasks):
@@ -57,12 +49,9 @@ def get_student_name_from_chat(chat_id):
     return MyBot.students.get(chat_id, None)
 
 
-obj = DBAnalyzer()
-
-
 async def periodic_update_loop(app, interval, USERS):
     while True:
-        print("Running periodic DB update...")
+        logging.info("Running periodic DB update...")
         try:
             for student_name in USERS.keys():
                 nums = doneOrNot(student_name=student_name)
@@ -71,14 +60,18 @@ async def periodic_update_loop(app, interval, USERS):
                 logging.info(f"Updating bot_data[{chat_id}] = {formatted_message}")
                 app.bot_data[f"custom_message_{chat_id}"] = formatted_message
         except Exception as e:
-            print(f'ERROR during periodic update: {e}')
+            logging.exception(f'ERROR during periodic update: {e}')
         await asyncio.sleep(interval)
 
 
-def start_bot():
-    asyncio.run(periodic_update_loop(app, interval=60, USERS=USERS))
+async def main():
+    app = MyBot.run_bot(TOKEN)
 
+    asyncio.create_task(periodic_update_loop(app, interval=60, USERS=USERS))
+
+    await app.run_polling()
+
+
+# 🚀 Запуск
 if __name__ == '__main__':
-    threading.Thread(target=start_bot, daemon=True).start()
-
-    app.run_polling()
+    asyncio.run(main())
