a73c5ee2c76bc9d8a71072c565a3aabf554a9d1e
kosokolovsky <kosokolovsky@icloud.com>
2025-04-06 22:49:54 +0200

    logging

--- src/run_bot.py
+++ src/run_bot.py
@@ -54,10 +54,11 @@ async def periodic_update_loop(app, interval, USERS):
         logging.info("Running periodic DB update...")
         try:
             for student_name in USERS.keys():
+                logging(student_name)
                 nums = doneOrNot(student_name=student_name)
                 formatted_message = format_missing_tasks_markdown(nums)
                 chat_id = USERS[student_name]
-                logging.info(f"Updating bot_data[{chat_id}] = {formatted_message}")
+                # logging.info(f"Updating bot_data[{chat_id}] = {formatted_message}")
                 app.bot_data[f"custom_message_{chat_id}"] = formatted_message
         except Exception as e:
             logging.exception(f'ERROR during periodic update: {e}')
@@ -72,6 +73,5 @@ async def main():
     await app.run_polling()
 
 
-# 🚀 Запуск
 if __name__ == '__main__':
     asyncio.run(main())
